fe898c964e61c2ba07dfb31cc30a4bdbac13a348
ct1150 <ct1150@gmail.com>
2020-12-07 17:03:25 +0800

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -1,8 +1,15 @@
 return {
   no_consumer = true,
   fields = {
-    roles = {type = "array", require = true},
+    need_role = {type = "boolean", default = false},
+    roles = {type = "array"},
     uri_name = {type = "string", default = "jwt"},
-    cookie_name = {type = "string", default = "jwt"}
+    cookie_name = {type = "string", default = "authToken"},
+	single_device = {type = "boolean", default = false},
+	redis_host = {type="string"},
+	redis_port = {type="number", default = 6379},
+	redis_pass = {type="string"},
+	redis_db = {type="number"},
+	redis_hash = {type="string"}
   }
 }
